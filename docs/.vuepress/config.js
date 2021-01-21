e08e2818b950f135a3ec28900c88a0c4af138fe4
Peter Krauß <peter.krauss@kit.edu>
2021-01-21 11:43:43 +0100

    Initial commit from mothership.

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+// .vuepress/config.js
+module.exports = {
+    title: 'Chemotion',
+    themeConfig: {
+        sidebar: ['/', 'docs'],
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Chemotion', link: 'https://www.chemotion.net' },
+        ],
+    },
+}
